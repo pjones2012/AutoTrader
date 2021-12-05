8913e143b14548484e258823f03d44a7e2a39e8a
pjones2012 <portiamelissa90@gmail.com>
2021-12-05 10:04:23 -0600

    improved db connetion error handling

--- server/DBMS.ts
+++ server/DBMS.ts
@@ -4,10 +4,16 @@ const client = new Client({
   user: 'postgres',
   host: '',
   database: 'investcrypto',
-  password: 'password',
+  password: '',
   port: 5432,
 })
-client.connect();
+client.connect(err => {
+  if (err) {
+    console.error('connection error', err.stack)
+  } else {
+    console.log('connected')
+  }
+});
 
 var findOne = (name, cb)=>{
   //console.log(name);
